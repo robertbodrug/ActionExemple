1c0ae1b2a42875cc9ae11e69ec7fa0c0075a3f82
robertbodrug <robertobodrug@gmail.com>
2024-04-08 04:59:08 +0200

    test

--- src/main/java/com/example/SimpleCalculator.java
+++ src/main/java/com/example/SimpleCalculator.java
@@ -15,7 +15,7 @@ public class SimpleCalculator {
 
     public double divide(int a, int b) {
         if (b == 0) {
-            throw new IllegalArgumentException("Cannot divide by 0");
+            throw new IllegalArgumentException("Cannot divide by zero");
         }
         return (double) a / b;
     }
