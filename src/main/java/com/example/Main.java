ce13ea2e2936414507734094c949e3e91e881e0f
robertbodrug <robertobodrug@gmail.com>
2024-04-08 04:43:20 +0200

    test3

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello df world!");
+        System.out.println("Hello my world!");
     }
 }
